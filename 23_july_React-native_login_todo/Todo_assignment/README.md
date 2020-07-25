f3bb1569f2b0b16625a459c3c4b18a143e285150
mukesh <sutharm786@gmail.com>
2020-07-25 15:23:17 +0530

    react-native app

---
+++ 23_july_React-native_login_todo/Todo_assignment/README.md
@@ -0,0 +1,5 @@
+# newsapp-rn
+
+- Clone the repo
+- npm install
+- npm run android / npm run ios / npm start
---
+++ 23_july_React-native_login_todo/Todo_assignment/src/component/Login.js
@@ -0,0 +1,79 @@
+import  React,{ useState, useEffect }  from "react";
+import { Text, View ,TextInput, Button, StyleSheet } from "react-native";
+import {getValue, setUserName} from "./StoreDB"
+
+const Login = ({navigation}) =>{
+      const [email, setEmail] = useState("");
+
+     
+      useEffect(  ()=>{
+            username_exists();
+       },[]);
+ 
+     
+      const username_exists = async ()=>{
+            const username = await getValue("username"); 
+            if(username)
+            {
+                  navigation.navigate('Todo');
+            }
+      }
+
+
+      const login =()=>
+      {
+            if(email)
+            {
+
+                  setUserName(key = "username", email);
+                  navigation.navigate('Todo');      
+            }else
+            {
+                  alert("Please insert username!");
+            }      
+      }
+
+
+      return (
+            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Text style={style.title}>Welcome to</Text>
+            <Text style={style.title}>Todo!!!</Text>
+            <Text style={style.label}>Please enter your email id to login</Text>
+            <TextInput style={style.textbox} onChangeText={(text)=>{ setEmail(text)} } placeholder="john.wilson@example.com"></TextInput>
+            <Button style={style.submit} onPress={login}  title="Login" ></Button>
+          </View>
+      );
+};
+
+const style = StyleSheet.create({
+      title:{
+            fontSize:40,
+            fontWeight:"700",
+            
+            color:"#444"
+      },
+      label:{
+            fontSize:12,
+            marginTop:20,
+            fontWeight:"400",
+            color:"#000"},
+      textbox:{
+            fontSize:15,
+            padding:5,
+            borderColor:"#777",
+            borderWidth:2,
+            borderRadius:8,
+            margin:15,
+            width:"70%",
+            textAlign:"center"
+            
+      },
+      submit:{
+            borderRadius:30,
+            height:60,
+            width:"50%"
+      }
+});
+
+
+export default Login;
