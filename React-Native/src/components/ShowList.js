8792a976c30532a4707d36459967bf17b59d1096
neeraj5394 <45269534+neeraj5394@users.noreply.github.com>
2020-08-04 03:23:58 +0530

    Add files via upload

---
+++ React-Native/src/components/ShowList.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { ImageBackground, Text, View, TouchableOpacity, StyleSheet } from "react-native";
+
+const ShowList = (data) => {
+    return (
+        <View>
+                    
+                        
+                    <Text>{data}</Text>
+                    
+                </View>
+    );
+}
+
+
+export default ShowList;
