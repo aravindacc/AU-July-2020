0e32494cee96520d85492a3fba6006b87cc4ede2
sukanya <sukanya18109@iiitd.ac.in>
2020-08-04 09:20:47 +0530

    RestAPI-Evening Assignment added.

--- ReactNative-MornEve/src/components/TodoCard.js
+++ ReactNative-MornEve/src/components/TodoCard.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import React from "react";
 import { View } from "react-native";
 
@@ -8,4 +9,16 @@ const TodoCard = (props) => {
         </View>);
 }
 
+=======
+import React from "react";
+import { View } from "react-native";
+
+const TodoCard = (props) => {
+    return (
+        <View style={{margin:10}}>
+            <Text>{props.note}</Text>
+        </View>);
+}
+
+>>>>>>> Stashed changes
 export default TodoCard;
